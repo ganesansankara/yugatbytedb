b19400dcbdbd06e630c50539d8f8849484711e9e
ganesansankara <ganesan.sankara@gmail.com>
2021-07-26 18:34:05 +0000

    m

--- .devcontainer/afterscripts/supersetinit.sh
+++ .devcontainer/afterscripts/supersetinit.sh
@@ -4,7 +4,8 @@ pip3 install apache-superset
 pip3 install pyexasol
 
 #SQLALCHEMY needs unixodbc. Please not use apt to install. Do not use apt-get
-sudo apt-get update &&  apt update && apt install -y unixodbc unixodbc-dev
+# already done in dockerfile
+#sudo apt-get update &&  sudo apt update && sudo apt install -y unixodbc unixodbc-dev
 
 #Install exasol ODBC Driver first
 export odbcfile=exasol-odbc.tar.gz
@@ -17,22 +18,5 @@ tar -xzf ${odbcfile}
 (cd  EXASolution_ODBC-*; ./config_odbc --mode config --force --host DEMODB.EXASOL.COM --user PUB3715 --password=NbMCCidzA )
 sudo cp EXASolution_ODBC-7.0.11/lib/linux/x86_64/*  /usr/lib/x86_64-linux-gnu/
 
-
 # Install sqlalchmey package for exasol next
-pip3 install  sqlalchemy-exasol
-
-superset db upgrade
-superset fab create-admin \
-               --username admin \
-               --password admin \
-               --firstname Superset \
-               --lastname Admin \
-               --email admin@superset.com 
-superset init
-
-# Test script
-#python sqlalchemyexasol-test.py
-
-#superset run -h 0.0.0.0 -p 8080
-
-#exa+pyodbc://PUB3715:NbMCCidzA@DEMODB.EXASOL.COM:8563/PUB3715?CONNECTIONLCALL=en_US.UTF-8&driver=/usr/lib/x86_64-linux-gnu/libexaodbc-uo2214lv2.so
+pip3 install sqlalchemy-exasol
